e5ffe8b86d6c8beb5a8824aa6ca7789ffca670d9
Divjot Singh Arora <aroradivjotsingh@gmail.com>
2022-12-29 17:11:18 +0530

    Heloo

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-const ATLAS_URL="mongodb+srv://admin:admin123@quiz.txg27yr.mongodb.net/?retryWrites=true&w=majority"
+const ATLAS_URL="mongodb+srv://SOSTECH:sostechincc22@sosquiz.euz9i07.mongodb.net/?retryWrites=true&w=majorityy"
 
 export default async function connect(){
     await mongoose.connect(ATLAS_URL)
